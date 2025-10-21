f0fa212a3891b81dd012051ed572ce1287dbffb0
Prashanthi <pgeesala@bu.edu>
2025-10-21 00:36:08 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/GeesalaPrashanthi.github.io/",
   server: {
     base: "/",
     host: "::",
